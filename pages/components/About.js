44f5416a0ea0ab9d5dc2fbface76f41931974edb
Christie Cardoso <christiecardoso@hotmail.com>
2023-08-27 19:22:46 -0300

    edit about icons

--- pages/components/About.js
+++ pages/components/About.js
@@ -4,10 +4,10 @@ import Logo from '../../public/logo.png';
 
 export default function AboutUs() {
   return (
-    <div className="relative isolate overflow-hidden py-24 sm:py-32">
+    <section className="relative isolate overflow-hidden py-24 sm:py-32">
       <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
         <div className="px-4 mb-10 md:text-center md:mb-20">
-          <h2 className="pb-2 text-2xl font-bold text-gray-800 md:text-4xl dark:text-gray-300">
+          <h2 className="pb-2 text-2xl font-bold text-gray-900 md:text-4xl dark:text-gray-600">
             Sobre
           </h2>
           <div className="flex w-32 mt-1 mb-6 overflow-hidden rounded md:mx-auto md:mb-14">
@@ -21,28 +21,30 @@ export default function AboutUs() {
         </div>
         <div className="flex flex-wrap items-center">
           <div className="w-full px-4 mb-10 md:w-1/2 lg:mb-0 ">
-            <h2 className="mb-4 text-2xl font-bold text-gray-700 dark:text-gray-300">
+            <h2 className="mb-4 text-2xl font-bold text-gray-900">
               Bem-vindo à Estofados 10/10, onde a excelência encontra a inovação na arte de limpar e higienizar estofados.
             </h2>
-            <p className="mb-4 text-base leading-7 text-gray-500 dark:text-gray-400">
+            <p className="mb-4 text-base leading-7 text-gray-500">
               Nossa paixão por elevar o conforto e a beleza dos espaços nos impulsiona a oferecer serviços de limpeza e cuidados excepcionais para estofados de todos os tipos. Somos mais do que apenas uma empresa de limpeza - somos criadores de ambientes que transformam suas peças de estofado em obras-primas impecáveis.
             </p>
-            <ul className="mb-10">
-              <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
-                <span className="text-blue-500 dark:text-blue-400">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
-                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
-                  </svg>
-                </span>
-                Value for money
-              </li>
-              <li className="flex items-center mb-4 text-base text-gray-600 dark:text-gray-400">
-                <span className="mr-3 text-blue-500 dark:text-blue-400">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trophy" viewBox="0 0 16 16">
+            <ul className="columns-2">
+              <li className="flex items-center mb-4 text-base text-gray-600">
+                <span className=" mr-3 text-blue-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-trophy" viewBox="0 0 20 16">
                     <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z" />
                   </svg>
                 </span>
-                Support Team
+                <span className="text-3xl text-blue-700 mr-3">2</span>
+                <span>Anos de experiência</span>
+              </li>
+              <li className="flex items-center mb-4 text-base text-gray-600">
+                <span className="mr-3 text-blue-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-people" viewBox="0 0 20 16">
+                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                  </svg>
+                </span>
+                <span className="text-3xl text-blue-700 mr-3">+50</span>
+                <span>Clientes Satisfeitos</span>
               </li>
             </ul>
           </div>
@@ -53,12 +55,9 @@ export default function AboutUs() {
               src={Logo}
               alt="Logo Estofados 10/10"
             />
-            <div className="absolute top-0 right-0 items-center justify-center hidden -mt-16 lg:inline-flex">
-
-            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
