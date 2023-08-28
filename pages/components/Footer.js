226347f060917fea046fa1395e6e1128f5c22ef2
Christie Cardoso <christiecardoso@hotmail.com>
2023-08-28 12:03:33 -0300

    edit footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -3,32 +3,32 @@ import Image from 'next/image';
 import Logo from '../../public/logo.png'
 export default function Footer() {
   return (
-    <section className="flex flex-col lg:justify-between font-poppins ">
-      <div className="w-full bg-blue-900 pt-5 dark:bg-gray-900 ">
+    <section className="flex flex-col lg:justify-between">
+      <div className="w-full bg-gray-200 pt-5">
         <div className="flex-1 px-4 py-4 lg:py-0">
-          <div className="flex flex-wrap py-10 -mx-3">
-            <div className="px-3 flex-col">
+          <div className="flex justify-evenly items-center flex-wrap py-10 -mx-3">
+            <div className="px-20 pb-8">
               <Image
                 width={200}
                 height={200}
                 src={Logo}
                 alt="Logo Estofados 10/10"
               />
-              <div className="flex  justify-center items-center">
-                <a className="inline-block mr-5 text-gray-800 hover:text-blue-700 dark:text-gray-400 dark:hover:text-blue-400"
+              <div className="flex  justify-center items-center text-gray-800">
+                <a className="inline-block mr-5 hover:text-blue-900"
                   href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-6 h-6 bi bi-facebook " viewBox="0 0 16 16">
                     <path
                       d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                   </svg>
                 </a>
-                <a className="inline-block mr-5 text-gray-800 dark:text-gray-400 hover:text-green-400 dark:hover:text-green-400"
+                <a className="inline-block mr-5 hover:text-green-400"
                   href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
                     <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
                   </svg>
                 </a>
-                <a className="inline-block text-gray-800 dark:text-gray-400 hover:text-pink-400 dark:hover:text-pink-400 "
+                <a className="inline-block hover:text-pink-500"
                   href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-6 h-6 bi bi-instagram" viewBox="0 0 16 16">
                     <path
@@ -37,55 +37,55 @@ export default function Footer() {
                 </a>
               </div>
             </div>
-            <div className="w-full px-4 md:w-1/4 lg:w-2/12 mb-11 lg:mb-0">
-              <h2 className="mb-4 text-xl font-bold text-gray-200 dark:text-gray-400">Navegue </h2>
+            <div className="w-full px-4 md:w-1/4 lg:w-2/12 mb-11 lg:mb-0 stroke-slate-800">
+              <h2 className="mb-4 text-xl font-bold">Navegue </h2>
               <ul>
                 <li className="flex items-center mb-4 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 text-white dark:text-gray-400 bi bi-chevron-right" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 bi bi-chevron-right" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
                   </svg>
-                  <a href="#" className="inline-block text-base font-normal text-gray-300 dark:text-gray-400">Home</a>
+                  <a href="#" className="inline-block text-base font-normal">Home</a>
                 </li>
                 <li className="flex items-center mb-4 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 text-white dark:text-gray-400 bi bi-chevron-right" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 bi bi-chevron-right" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
                   </svg>
-                  <a href="#" className="inline-block text-base font-normal text-gray-300 dark:text-gray-400">Serviços</a>
+                  <a href="#" className="inline-block text-base font-normal ">Serviços</a>
                 </li>
                 <li className="flex items-center mb-4 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 text-white dark:text-gray-400 bi bi-chevron-right" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-3 h-3 mr-1 bi bi-chevron-right" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
                   </svg>
-                  <a href="#" className="inline-block text-base font-normal text-gray-300 dark:text-gray-400">Avaliações</a>
+                  <a href="#" className="inline-block text-base font-normal">Avaliações</a>
                 </li>
               </ul>
             </div>
 
-            <div className="w-full px-4 mb-2 lg:mb-0 md:w-1/3 lg:w-4/12">
-              <h2 className="mb-4 text-lg font-bold text-gray-200 dark:text-gray-400">Contact Info</h2>
+            <div className="w-full px-4 mb-2 lg:mb-0 md:w-1/3 lg:w-4/12 text-gray-800">
+              <h2 className="mb-4 text-lg font-bold ">Contact Info</h2>
               <p className="flex items-center mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-1 text-gray-300 dark:text-gray-400 bi bi-geo-alt " viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-1 bi bi-geo-alt " viewBox="0 0 16 16">
                   <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"></path>
                   <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
                 </svg>
-                <span className="text-gray-300 dark:text-gray-400">Kathmandu, Nepal</span>
+                <span>Kathmandu, Nepal</span>
               </p>
               <p className="flex items-center mb-4 ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-2 text-gray-300 dark:text-gray-400 bi bi-envelope" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-2 bi bi-envelope" viewBox="0 0 16 16">
                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"></path>
                 </svg>
-                <span className="text-gray-300 dark:text-gray-400">info@gmail.com</span>
+                <span>info@gmail.com</span>
               </p>
               <p className="flex items-center mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-2 text-gray-300 dark:text-gray-400 bi bi-telephone" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-4 h-4 mr-2 bi bi-telephone" viewBox="0 0 16 16">
                   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"></path>
                 </svg>
-                <span className="text-gray-300 dark:text-gray-400">+977-9836736352</span>
+                <span>+977-9836736352</span>
               </p>
             </div>
           </div>
         </div>
-        <div className=" py-2 text-center text-gray-200 dark:bg-gray-800">
+        <div className=" py-2 text-center text-gray-200 bg-gray-700">
           <span>© 2023 Estofados 10/10 | Todos os direitos reservados.</span>
           <p className='flex justify-center gap-2 items-center'>Dev by{" "}<a
             href="https://github.com/ChristieCardoso"
